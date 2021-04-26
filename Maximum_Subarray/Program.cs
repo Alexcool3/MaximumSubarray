4739bda470812f54cc548a1aba8c250e139d610c
Alexcool3 <36537085+Alexcool3@users.noreply.github.com>
2021-04-26 19:12:22 +0200

    Code clean up

--- Maximum_Subarray/Program.cs
+++ Maximum_Subarray/Program.cs
@@ -9,21 +9,13 @@ namespace Maximum_Subarray
         {
             int size = 10000000;
             int[] A = RandomIntArray(size, 0, int.MaxValue);
-
-            //Stopwatch stopwatch = new Stopwatch();
-            //stopwatch.Start();
-            //FindMaximumSubarray(A, 0, A.Length-1);
-            //Kadane(A);
-            //stopwatch.Stop();
-            //Console.WriteLine($"elapsed Time:{stopwatch.ElapsedMilliseconds}");
             Console.WriteLine($"FindMaximumSubarray:{FindMaximumSubarray(A, 0, A.Length-1)}");
-            Console.WriteLine($"MasSubArraySum:{maxSubArraySum(A, 0, A.Length-1)}");;
-            Console.WriteLine($"Kadane Other All Negative:{maxSubArraySum(A, A.Length)}");
             Console.WriteLine($"Kadane:{Kadane(A)}");
             Console.ReadKey();
             
         }
 
+        // Returns integer array with specified size and element values within specified range.
         public static int[] RandomIntArray(int size, int minimum = int.MinValue, int maximum = int.MaxValue)
         {
             int[] A = new int[size];
@@ -38,6 +30,7 @@ namespace Maximum_Subarray
             return A;
         }
 
+        // Kadane's algorithm
         public static int Kadane(int[] A)
         {
             int local_max = 0;
@@ -55,21 +48,7 @@ namespace Maximum_Subarray
             return global_max;
         }
 
-        // Kadane's other.
-        static int maxSubArraySum(int[] a, int size)
-        {
-            int max_so_far = a[0];
-            int curr_max = a[0];
-
-            for (int i = 1; i < size; i++)
-            {
-                curr_max = Math.Max(a[i], curr_max + a[i]);
-                max_so_far = Math.Max(max_so_far, curr_max);
-            }
-
-            return max_so_far;
-        }
-
+        // Find Max Crossing Subarray algorithm
         public static (int, int, int) FindMaxCrossingSubarray(int[] A, int low, int mid, int high)
         {
             int left_sum = int.MinValue;
@@ -101,6 +80,7 @@ namespace Maximum_Subarray
             return (max_left, max_right, Math.Max(left_sum + right_sum, Math.Max(left_sum, right_sum)));
         }
 
+        //Find Max Subarray algorithm.
         public static (int, int, int) FindMaximumSubarray(int[] A, int low, int high)
         {
             if (high == low) 
@@ -127,61 +107,6 @@ namespace Maximum_Subarray
                     return (cross_low, cross_high, cross_sum);
                 }
             }
-        }
-        
-        static int maxCrossingSum(int[] arr, int l, int m, int h)
-        {
-            // Include elements on left of mid.
-            int sum = 0;
-            int left_sum = int.MinValue;
-            for (int i = m; i >= l; i--)
-            {
-                sum = sum + arr[i];
-                if (sum > left_sum)
-                    left_sum = sum;
-            }
-
-            // Include elements on right of mid
-            sum = 0;
-            int right_sum = int.MinValue;
-            ;
-            for (int i = m + 1; i <= h; i++)
-            {
-                sum = sum + arr[i];
-                if (sum > right_sum)
-                    right_sum = sum;
-            }
-
-            // Return sum of elements on left
-            // and right of mid
-            // returning only left_sum + right_sum will fail for
-            // [-2, 1]
-            return Math.Max(left_sum + right_sum,
-                            Math.Max(left_sum, right_sum));
-        }
-
-        // Returns sum of maxium sum subarray
-        // in aa[l..h]
-        static int maxSubArraySum(int[] arr, int l, int h)
-        {
-
-            // Base Case: Only one element
-            if (l == h)
-                return arr[l];
-
-            // Find middle point
-            int m = (l + h) / 2;
-
-            /* Return maximum of following three
-            possible cases:
-            a) Maximum subarray sum in left half
-            b) Maximum subarray sum in right half
-            c) Maximum subarray sum such that the
-            subarray crosses the midpoint */
-            return Math.Max(
-                Math.Max(maxSubArraySum(arr, l, m),
-                         maxSubArraySum(arr, m + 1, h)),
-                maxCrossingSum(arr, l, m, h));
         }
     }
 }
